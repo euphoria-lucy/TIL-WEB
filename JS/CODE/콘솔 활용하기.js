5a9148adf527656ccb6f618e985896835be6fa7c
misung <voielactee7@naver.com>
2023-10-21 22:12:01 +0900

    :memo: 콘솔 활용하기

---
+++ JS/CODE/콘솔 활용하기.js
@@ -0,0 +1,16 @@
+// console.log(  로그에_출력할_값  );
+
+// console.log(  값1, 값2, 값3...  );
+
+// 브라우저 콘솔 활용
+console.log("Hello World!");
+console.log(7);
+console.log(true);
+console.log({ name: "이정현", age: 22 });
+console.log("Hi!", 65, true, [2, 7, 9]);
+
+// 콘솔 기능
+console.log("로그 - 기본적인 출력");
+console.info("로그 - 기능적으로는 log와 같음. 사용하기에 따라 용도 구분 가능");
+console.warn("경고 - 문제가 될 수 있는 부분");
+console.error("오류 - 에러 발생 상황");
