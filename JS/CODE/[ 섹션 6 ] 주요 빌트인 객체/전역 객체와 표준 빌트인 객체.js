1ec93fc3463cba16b7339e84a577ffec76958ea7
misung <voielactee7@naver.com>
2024-01-30 17:05:48 +0900

    [JS] 전역 객체와 표준 빌트인 객체

---
+++ JS/CODE/[ 섹션 6 ] 주요 빌트인 객체/전역 객체와 표준 빌트인 객체.js
@@ -0,0 +1,29 @@
+/* var myGlobalVar = 1;
+const myGlobalConst = 1;
+
+function myGlobalFunc() {}
+
+console.log(
+  globalThis.myGlobalVar,
+  globalThis.myGlobalConst,
+  globalThis.myGlobalFunc
+);
+
+const str = "abcde";
+console.log(str.length, str.toUpperCase(), str[0]);
+
+const str = new String("abcde");
+const num = new Number(123.4567);
+const bool = new Boolean(true);
+
+console.log(typeof str, str);
+console.log(typeof num, num);
+console.log(typeof bool, bool);
+
+const str = new String("abcde");
+const num = new Number(123.4567);
+const bool = new Boolean(true);
+
+console.log(str.valueOf());
+console.log(num.valueOf());
+console.log(bool.valueOf()); */
